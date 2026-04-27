4f9ca70918deebb005b83cf520ac79142ce57c48
dbauhue1708 <dbauhue1708@g.educaand.es>
2026-04-27 13:20:27 +0200

    creacion elementos de cada receta hecho

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,16 +12,35 @@ listaRecetasXml.forEach(nodo => {
 
     let receta = {
         id: nodo.getAttribute("id"),
+        categoria: nodo.querySelector("categoria").textContent,
+        nombre: nodo.querySelector("dificultad").textContent,
         tiempo: Number(nodo.querySelector("tiempo").textContent),
         dificultad: nodo.querySelector("dificultad").textContent
     }
     listaRecetasJson.push(receta)
     })
 
-const tablaIndex = document.querySelector()
+const tablaIndex = document.querySelector("table")
 
 listaRecetasJson.forEach(receta => {
 
-    const tr
+    const tr = document.createElement("tr")
+    const tdCategoria= document.createElement("td")
+    const tdNombre = document.createElement("td")
+    const tdTiempo= document.createElement("td")
+    const tdDificultad = document.createElement("td")
 
-    })
+    tdCategoria.textContent = receta.categoria
+    tr.appendChild(tdCategoria)
+
+    tdNombre.textContent = receta.nombre
+    tr.appendChild(tdNombre)
+
+    tdTiempo.textContent = receta.tiempo
+    tr.appendChild(tdTiempo)
+
+    tdDificultad.textContent = receta.dificultad
+    tr.appendChild(tdDificultad)
+
+    tablaIndex.appendChild(tr)
+})
