eb2259583ca2bb6a407ab429576b7bcae89a7379
dbauhue1708 <dbauhue1708@g.educaand.es>
2026-04-27 13:52:21 +0200

    añadido thead y tbody

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,54 @@
-const xmlRecetas = await fetch("recetas.xml")
-const xmlRecetasTexto = await  xmlRecetas.text()
+async function cargarXml(){
+    const xmlRecetas = await fetch("recetas.xml")
+    const xmlRecetasTexto = await  xmlRecetas.text()
 
-const parser = new DOMParser()
-const documentoRecetas = parser.parseFromString(xmlRecetasTexto, "application/xml")
-const listaRecetasXml = documentoRecetas.querySelectorAll("receta")
-const listaRecetasJson = []
+    const parser = new DOMParser()
+    const documentoRecetas = parser.parseFromString(xmlRecetasTexto, "application/xml")
+    const listaRecetasXml = documentoRecetas.querySelectorAll("receta")
+    const listaRecetasJson = []
 
-listaRecetasXml.forEach(nodo => {
+    listaRecetasXml.forEach(nodo => {
 
-    let receta = {
-        id: nodo.getAttribute("id"),
-        categoria: nodo.querySelector("categoria").textContent,
-        nombre: nodo.querySelector("dificultad").textContent,
-        tiempo: Number(nodo.querySelector("tiempo").textContent),
-        dificultad: nodo.querySelector("dificultad").textContent
-    }
-    listaRecetasJson.push(receta)
+        let receta = {
+            id: nodo.getAttribute("id"),
+            categoria: nodo.querySelector("categoria").textContent,
+            nombre: nodo.querySelector("dificultad").textContent,
+            tiempo: Number(nodo.querySelector("tiempo").textContent),
+            dificultad: nodo.querySelector("dificultad").textContent
+        }
+        listaRecetasJson.push(receta)
     })
 
-const tablaIndex = document.querySelector("table")
+    const tablaIndex = document.querySelector("tbody")
 
-listaRecetasJson.forEach(receta => {
+    listaRecetasJson.forEach(receta => {
 
-    const tr = document.createElement("tr")
-    const tdCategoria= document.createElement("td")
-    const tdNombre = document.createElement("td")
-    const tdTiempo= document.createElement("td")
-    const tdDificultad = document.createElement("td")
+        const tr = document.createElement("tr")
+        const tdCategoria= document.createElement("td")
+        const tdNombre = document.createElement("td")
+        const tdTiempo= document.createElement("td")
+        const tdDificultad = document.createElement("td")
 
-    tdCategoria.textContent = receta.categoria
-    tr.appendChild(tdCategoria)
+        tdCategoria.textContent = receta.categoria
+        tr.appendChild(tdCategoria)
+        if (receta.categoria === "Entrante") {
+            tdCategoria.classList.add("badge-entrante")
+        } else if (receta.categoria === "Principal") {
+            tdCategoria.classList.add("badge-principal")
+        } else if (receta.categoria === "Postre"){
+            tdCategoria.classList.add("badge-postre")
+        }
 
-    tdNombre.textContent = receta.nombre
-    tr.appendChild(tdNombre)
+        tdNombre.textContent = receta.nombre
+        tr.appendChild(tdNombre)
 
-    tdTiempo.textContent = receta.tiempo
-    tr.appendChild(tdTiempo)
+        tdTiempo.textContent = receta.tiempo
+        tr.appendChild(tdTiempo)
 
-    tdDificultad.textContent = receta.dificultad
-    tr.appendChild(tdDificultad)
+        tdDificultad.textContent = receta.dificultad
+        tr.appendChild(tdDificultad)
 
-    tablaIndex.appendChild(tr)
-})
+        tablaIndex.appendChild(tr)
+    })
+}
+cargarXml()
